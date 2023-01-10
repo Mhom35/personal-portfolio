1253deb5b1587ab64220fb1513a03beb76d30d9f
Mitchell H <mitch@Mitchells-MBP-2.attlocal.net>
2023-01-09 18:12:53 -0800

    added config for glitch

--- src/App.js
+++ src/App.js
@@ -13,6 +13,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+
+  const express = require("express");
+  const path = require("path");
+
+  const app = express();
+
+  app.use(express.static(path.join(__dirname, "dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Server Started");
+  });
+
   return (
     <div className="App">
       <NavBar />
