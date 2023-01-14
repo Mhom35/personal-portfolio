e1ccb9fdb7cd82f14c6113f492d9de5b12f7758f
Mitchell H <mitch@Mitchells-MBP-2.attlocal.net>
2023-01-13 17:18:43 -0800

    testing download

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import { Navbar, Nav, Container} from "react-bootstrap";
 import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 import github from '../assets/img/github.svg'
 import mitchellHomResume2023 from '../assets/img/mitchellHomResume2023.pdf';
 
